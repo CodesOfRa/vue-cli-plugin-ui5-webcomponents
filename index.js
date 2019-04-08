bc350700121aea288d338b9237644c5c45f7b9a6
CodesOfRa <ramona.biscoveanu@gmail.com>
2019-04-08 14:06:48 +0200

    :wrench: webpack config

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = (api, opts) => {}
+module.exports = (api, opts) => {
+    api.chaiWebpack(config => {
+        config
+            .module
+            .rule("loader")
+            .test(/\.(png|jpg|gif)$/)
+            .use("file-loader")
+            .loader("file-loader")
+    })
+}
