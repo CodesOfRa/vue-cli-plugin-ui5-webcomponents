60b25dce41aa6fada86ccf5e99b5d32703e78595
CodesOfRa <ramona.biscoveanu@gmail.com>
2019-04-10 10:02:23 +0200

    remove webpack config

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-module.exports = (api, opts) => {
-    api.chaiWebpack(config => {
-        config
-            .module
-            .rule("loader")
-            .test(/\.(png|jpg|gif)$/)
-            .use("file-loader")
-            .loader("file-loader")
-    })
-}
+module.exports = (api, opts) => {};
