c81ddc0f9995d0ffbb283e8a400ee866e6a61473
Ramona <ramona.biscoveanu@gmail.com>
2019-06-25 14:31:15 +0200

    Bump version (#4)
    * fix: bump version and add logo
    
    * fix: clean example

--- generator/index.js
+++ generator/index.js
@@ -1,35 +1,29 @@
-let ui5 = "\nVue.config.ignoredElements = [/^ui5-/];";
+let ui5 = '\nVue.config.ignoredElements = [/^ui5-/];'
 
 module.exports = (api, options, rootOptions) => {
-    api.extendPackage({
-        dependencies: {
-            "@ui5/webcomponents": "^0.9.0"
-        }
-    });
-    if (options.addExample) {
-        api.render("./template", {
-            ...options
-        });
+  api.extendPackage({
+    dependencies: {
+      '@ui5/webcomponents': '^0.13.0'
     }
-    api.onCreateComplete(() => {
-        // inject to main.js
-        const fs = require("fs");
-        const ext = api.hasPlugin("typescript")
-            ? "ts"
-            : "js";
-        const mainPath = api.resolve(`./src/main.${ext}`);
-        let contentMain = fs.readFileSync(mainPath, {encoding: "utf-8"});
-        const lines = contentMain
-            .split(/\r?\n/g)
-            .reverse();
+  })
+  if (options.addExample) {
+    api.render('./template', {
+      ...options
+    })
+  }
+  api.onCreateComplete(() => {
+    // inject to main.js
+    const fs = require('fs')
+    const ext = api.hasPlugin('typescript') ? 'ts' : 'js'
+    const mainPath = api.resolve(`./src/main.${ext}`)
+    let contentMain = fs.readFileSync(mainPath, { encoding: 'utf-8' })
+    const lines = contentMain.split(/\r?\n/g).reverse()
 
-        const lastImportIndex = lines.findIndex(line => line.match(/^import/));
-        lines[lastImportIndex] += ui5;
+    const lastImportIndex = lines.findIndex(line => line.match(/^import/))
+    lines[lastImportIndex] += ui5
 
-        contentMain = lines
-            .reverse()
-            .join("\n");
-        fs.writeFileSync(mainPath, contentMain, {encoding: "utf-8"});
-        api.exitLog("Successfully installed UI5 Web Components", "done");
-    });
-};
+    contentMain = lines.reverse().join('\n')
+    fs.writeFileSync(mainPath, contentMain, { encoding: 'utf-8' })
+    api.exitLog('Successfully installed UI5 Web Components', 'done')
+  })
+}
---
+++ generator/template/src/components/UI5Example.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ui5-shellbar primary-title="UI5 Web Components CLI Plugin Example"></ui5-shellbar>
+    <ui5-tabcontainer show-overflow>
+      <ui5-tab v-for="tab in tabs" :key="tab" :text="tab">
+        <ui5-button @click="onClick(tab)">{{tab}}</ui5-button>
+        <ui5-icon v-show="tab==='Home'" src="sap-icon://add-employee"></ui5-icon>
+      </ui5-tab>
+    </ui5-tabcontainer>
+  </div>
+</template>
+<script>
+import "@ui5/webcomponents/dist/Button";
+import "@ui5/webcomponents/dist/ShellBar";
+import "@ui5/webcomponents/dist/Icon";
+import "@ui5/webcomponents/dist/TabContainer";
+import "@ui5/webcomponents/dist/Tab";
+export default {
+  data: function() {
+    return {
+      tabs: ["Home", "About", "News", "Contacts"]
+    };
+  },
+  methods: {
+    onClick(text) {
+      console.log(text);
+    }
+  }
+};
+</script>
